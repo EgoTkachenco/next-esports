943424b1cca822c60bb010446b1d221ed428a397
EgoTkachenco <egortkachenco@gmail.com>
2021-08-17 13:54:07 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -3,12 +3,13 @@ module.exports = {
   reactStrictMode: true,
   distDir: 'build',
   basePath: '/next-esports',
-  assetPrefix: '/next-esports',
+  assetPrefix: '/next-esports/',
   images: {
     loader: "imgix",
     path: process.env.NODE_ENV === 'production' ? 'https://egotkachenco.github.io/next-esports/out/' : 'http://localhost:3000/',
   },
-  // env: {
-  //   BACKEND_URL: '/next-esports',
-  // },
+  
+  env: {
+    BACKEND_URL: '/next-esports',
+  },
 }
