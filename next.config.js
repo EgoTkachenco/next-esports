3e71388a22f79bad4dde74755a009ce699cacdc0
EgoTkachenco <egortkachenco@gmail.com>
2021-08-17 13:30:01 +0300

    build fix

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ console.log(process.env.NODE_ENV)
 module.exports = {
   reactStrictMode: true,
   distDir: 'build',
+  basePath: '/next-esports/docs',
   images: {
     loader: "imgix",
     path: process.env.NODE_ENV === 'production' ? 'https://egotkachenco.github.io/next-esports/out/' : 'http://localhost:3000/',
